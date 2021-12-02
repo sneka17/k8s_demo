0fd45da951e2c631d03931744ac45d49f9ddba25
sneka17 <kungumasnekam@gmail.com>
2021-12-02 15:46:48 +0530

    ingress commit

---
+++ bootstrap-4.3.1-dist/javascript.js
@@ -0,0 +1,9 @@
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+
+var myApp= angular.module("myModule",[]);
+
+
+myApp.controller("myController", function($scope)
+{
+	$scope.message =" Java script Tutorial";
+});
